bfa57c984bb49cb9219d6bd1af9ebde7fdfc8e84
root <root@vmi380844.contaboserver.net>
2020-05-15 15:36:08 +0100

    Query support

--- public/js/index.js
+++ public/js/index.js
@@ -1,36 +1,17 @@
-const update = async (interval) => {
-	
-	let initialUpdate = await fetch("/api/status");
-	let initialResponse = await initialUpdate.json();
-	let tableData = {}
-	
-	initialResponse.servers.forEach((res, index, array) => {
-		
-		let table = document.getElementById("statusTable");
-		
-		tableData[res.id] = table.insertRow(index + 1)
-		tableData[res.id].name = tableData[res.id].insertCell(0)
-		tableData[res.id].description = tableData[res.id].insertCell(1)
-		tableData[res.id].state = tableData[res.id].insertCell(2)
-		
-		tableData[res.id].name.innerHTML = `${res.name}`
-		tableData[res.id].description.innerHTML = `${res.description}`
-		tableData[res.id].state.innerHTML = `${res.state == 1 ? "Online" : "Offline"}`
-		
-		if (res.state == 1) { tableData[res.id].state.style.color = "#7fff7f"}
-		else { tableData[res.id].state.style.color = "red"}
+let tableData = {};
 
-		let date = new Date(initialResponse.lastUpdated)
-		document.getElementById("lastUpdated").innerHTML = `Last updated: ${date.toString()}`
+fetch("/api/status").then(initialUpdate => {
 
-	})
-	
-	setInterval(async () => {
-		
-		let intUpdate = await fetch("/api/status");
-		let intResponse = await intUpdate.json();
-		
-		intResponse.servers.forEach((res) => {
+	initialUpdate.json().then(initialResponse => {
+
+		initialResponse.servers.forEach((res, index, array) => {
+			
+			let table = document.getElementById("statusTable");
+			
+			tableData[res.id] = table.insertRow(index + 1)
+			tableData[res.id].name = tableData[res.id].insertCell(0)
+			tableData[res.id].description = tableData[res.id].insertCell(1)
+			tableData[res.id].state = tableData[res.id].insertCell(2)
 			
 			tableData[res.id].name.innerHTML = `${res.name}`
 			tableData[res.id].description.innerHTML = `${res.description}`
@@ -38,16 +19,53 @@ const update = async (interval) => {
 			
 			if (res.state == 1) { tableData[res.id].state.style.color = "#7fff7f"}
 			else { tableData[res.id].state.style.color = "red"}
-			
+		
 		})
-		
-		let date = new Date(intResponse.lastUpdated)
-		document.getElementById("lastUpdated").innerHTML = `Last updated: ${date.toString()}`
+		if (initialResponse.queryEnabled) {
+			initialResponse.query.forEach((query, index, array) => {
 
-		console.log("Updated")
-		
-	}, interval)
+				tableData[query.id].online = tableData[query.id].insertCell(3)
+				tableData[query.id].version = tableData[query.id].insertCell(4)
+				tableData[query.id].players = tableData[query.id].insertCell(5)
 	
+				tableData[query.id].online.innerHTML = `${query.playersOn}/${query.playersMax}`
+				tableData[query.id].version.innerHTML = query.version
+	
+			})
+		}
+	})
+
+})
+
+const update = () => {
+
+	fetch("/api/status").then(intUpdate => {
+		intUpdate.json().then(intResponse => {
+			intResponse.servers.forEach((res) => {
+		
+				tableData[res.id].name.innerHTML = `${res.name}`
+				tableData[res.id].description.innerHTML = `${res.description}`
+				tableData[res.id].state.innerHTML = `${res.state == 1 ? "Online" : "Offline"}`
+				
+				if (res.state == 1) { tableData[res.id].state.style.color = "#7fff7f"}
+				else { tableData[res.id].state.style.color = "red"}
+				
+				let date = new Date();
+				document.getElementById("lastUpdated").innerHTML = `Last updated: ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} @ [${date.getHours() + 1}:${date.getMinutes() + 1}:${date.getSeconds() + 1}]`
+			})
+			if (intResponse.queryEnabled) {
+				intResponse.query.forEach((query, index, array) => {
+	
+					tableData[query.id].online.innerHTML = `${query.playersOn}/${query.playersMax}`
+					tableData[query.id].version.innerHTML = query.version
+		
+				})
+			}
+		})
+	})
+	
+
 }
 
-update(60000)
+	
+	
